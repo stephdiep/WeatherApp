e2bb3caef1b1cc33a1a82b6c49827d81c99da879
stephdiep <stephanie.diep002@gmail.com>
2021-12-13 08:01:43 -0500

    Remove hard-coded Montreal endpoint

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -10,8 +10,7 @@ import CoreLocation
 
 class WeatherManager {    
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-//        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=48532a5e16bd27acbb55cf0c9b778afc&units=metric") else { fatalError("Missing URL") }
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=48532a5e16bd27acbb55cf0c9b778afc&units=metric") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=48532a5e16bd27acbb55cf0c9b778afc&units=metric") else { fatalError("Missing URL") }
 
 
         let urlRequest = URLRequest(url: url)

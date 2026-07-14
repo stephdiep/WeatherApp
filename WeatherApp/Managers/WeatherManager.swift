14c3f0ec6db97cfd4d1aaf9ef2af687d65b3e8e7
Stephanie Diep <61625854+stephdiep@users.noreply.github.com>
2026-07-14 13:49:34 -0400

    Update WeatherManager.swift

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -12,7 +12,7 @@ class WeatherManager {
     // HTTP request to get the current weather depending on the coordinates we got from LocationManager
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
         // Replace YOUR_API_KEY in the link below with your own
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=48532a5e16bd27acbb55cf0c9b778afc&units=metric") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=<YOUR_API_KEY>&units=metric") else { fatalError("Missing URL") }
 
 
         let urlRequest = URLRequest(url: url)
